aeea446d700557b1118a728e16b61fec0eac2640
Guadalupe Payan Camacho <35412119+payangithub@users.noreply.github.com>
2022-11-02 11:27:24 -0700

    Se actualiza frontend  para agregar los ultimos detalles del diseño y funcionalidad de la pagina de PRODUCTOS DEL CAMPPO

--- FrontEnd/Campo/src/app/app-routing.module.ts
+++ FrontEnd/Campo/src/app/app-routing.module.ts
@@ -1,28 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, Router } from '@angular/router';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsuariosComponent } from './usuarios/usuarios.component';
-import { PayanComponent } from "./payan/payan.component";
+import { RouterModule, Routes } from '@angular/router';
+import { ProductosagricolasComponent } from './productosagricolas/productosagricolas.component';
 
-export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: '', component: PayanComponent },
-  { path: 'usuarios', component: UsuariosComponent },
-  { path: 'pay', component: PayanComponent },
+const routes: Routes = [
+    { path: 'principal', component: ProductosagricolasComponent },
+     { path: '', component: ProductosagricolasComponent },
 
-
-  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true, relativeLinkResolution: 'legacy' })],
-  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {
-  constructor(private router: Router) {
-    this.router.errorHandler = (error: any) => {
-      this.router.navigate(['']); // or redirect to default route
-    };
-  }
-}
+export class AppRoutingModule { }
--- FrontEnd/Campo/src/environments/environment.prod.ts
+++ FrontEnd/Campo/src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: true,
-  apiService: 'http://192.168.10.216:3000/api/productos/',
+  production: true
 };
